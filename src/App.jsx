4c286f82c5f1f2f662d3978b893fdb8bad422a79
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-05-20 14:50:10 +0200

    [UPD] Fix searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 
@@ -6,11 +6,21 @@ import { useEffect, useState } from 'react'
 function App() {
 
   const [search, setSearch] = useState("")
+  const [films, setFilms] = useState([]);
+  const query = encodeURIComponent(search)
 
   const searchedFilm = () => {
     axios
-      .get("https://api.themoviedb.org/3/discover/movie?api_key=e99307154c6dfb0b4750f6603256716d")
+      .get("https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d", {
+        params: {
+          query: search
+        }
+      })
       .then((response) => {
+        setFilms(response.data.results);
+        console.log("Risposta API:", response);
+        console.log("Risultati film:", response.data.results);
+
 
       })
   }
@@ -24,12 +34,10 @@ function App() {
             <h1>BOOLFLIX</h1>
           </div>
           <div className="col-12">
-            <input type="text" placeholder='Cerca...' value={search} />
-            <button type='submit' className="btn btn-primary">Cerca</button>
+            <input type="text" className='form-control' placeholder='Cerca...' value={search}
+              onChange={(e) => setSearch(e.target.value)} />
+            <button type='submit' className="btn btn-primary" onClick={searchedFilm}>Cerca</button>
           </div>
-
-
-
         </div>
       </div>
 
